0c438faa3d50a6170be8a598fe49214c1eee782a
Catalin Dumitru <colin.dumitru@gmail.com>
2016-04-02 22:12:38 +0300

    Updated the way the port is assigned to the app.

--- magic.js
+++ magic.js
@@ -47,7 +47,7 @@ app.get('/rooms', function (req, res) {
     );
 });
 
-var server = app.listen(8080, function () {
+var server = app.listen(process.env.PORT || 8080, function () {
     var host = server.address().address
     var port = server.address().port
 
